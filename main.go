2f450783de7725ad299df1908434bc7b4026a0fe
anandus <49789123+anandus0070@users.noreply.github.com>
2020-03-20 19:28:27 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbot.NewBotAPI("987206364:AAGKPNcUnwPjXQdJOBUJ9RPjo6HZ735CHGY")
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	fmt.Print("Authorized on account %s")
+
+	u := tgbot.NewUpdate(0)
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		msg := tgbot.NewMessage(update.Message.Chat.ID, "podi poori")
+		bot.Send(msg)
+	}
+}
