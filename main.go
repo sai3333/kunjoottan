e0fc2a6a2dea71601eaf8733f5a2de16cd8b97f0
anandus <49789123+anandus0070@users.noreply.github.com>
2020-03-21 05:16:06 -0700

    Update main.go

--- main.go
+++ main.go
@@ -24,9 +24,7 @@ func main() {
 		}
 
 		msg := tgbot.NewMessage(update.Message.Chat.ID)
-		var list = "fuck off ,
-		            funda mayir ,
-		            പോടീ പൂറി🤬🤬🤬🤬"
+		var list = "fuck, funda mayir , പോടീ പൂറി🤬🤬🤬🤬"
 		msg = list
 		bot.Send(list)
 		
